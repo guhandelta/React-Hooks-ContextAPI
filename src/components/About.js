2603ae15312f4b9247a84a7cc5e9dac00c49316a
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-05-28 16:30:15 -0400

    Created a component for the about page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='container'>
+            <h5>About Us</h5>
+            <p>
+                This a React Application developed using React Hooks
+      </p>
+        </div>
+    );
+};
+
+export default About;
